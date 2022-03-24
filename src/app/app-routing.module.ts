6c4223e95217fd0dfab3481a12e20214253f8a52
ceoxevello <87611512+ceoxevello@users.noreply.github.com>
2022-03-24 23:17:38 +0530

    first after setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TopNavComponent } from './desktop/components/top-nav/top-nav.component';
+import { IndexHomeComponent } from './desktop/views/index-home/index-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  {path: 'home', component: IndexHomeComponent, data: { title : "Xevello | India's leading Technology Consultancy services"}},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent = []
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './desktop/components/top-nav/top-nav.component';
+import { IndexHomeComponent } from './desktop/views/index-home/index-home.component';
+import { AllSvgsComponent } from './desktop/components/all-svgs/all-svgs.component';
+import { FooterComponent } from './desktop/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    IndexHomeComponent,
+    AllSvgsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
